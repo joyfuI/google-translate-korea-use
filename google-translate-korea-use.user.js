e6c3c094b258e9c197280eea9cb742b1787899af
joyfuI <jong970105@gmail.com>
2020-09-23 21:20:37 +0900

    http 주소 추가

--- google-translate-korea-use.user.js
+++ google-translate-korea-use.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name		Google Translate Korea Use
 // @namespace	https://joyfui.wo.tc/
-// @version		1
+// @version		2
 // @author		joyfuI
 // @description	대한민국에서 차단된 구글 번역의 URL 번역 기능을 사용할 수 있게 합니다.
 // @homepageURL	https://github.com/joyfuI/google-translate-korea-use
 // @downloadURL	https://raw.githubusercontent.com/joyfuI/google-translate-korea-use/master/google-translate-korea-use.user.js
 // @include		https://translate.google.*/translate*u=*
 // @include		https://translate.google.*.*/translate*u=*
+// @include		http://translate.google.*/translate*u=*
+// @include		http://translate.google.*.*/translate*u=*
 // @run-at		document-start
 // @grant		none
 // ==/UserScript==
